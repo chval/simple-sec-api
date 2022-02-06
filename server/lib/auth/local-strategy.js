8b62477e8095735a593e53592b5403aa7a068918
Vadym Nechval <blackchval@gmail.com>
2022-02-06 22:37:47 +0200

    Split auth route from main

---
+++ server/lib/auth/local-strategy.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const UserLogin = include('models/UserLogin');
+const Password = include('auth/Password');
+
+module.exports = new LocalStrategy(
+    {
+        usernameField: 'email',
+        passwordField: 'password'
+    },
+    // verify callback
+    async (email, password, done) => {
+        const userLogin = new UserLogin({ email });
+        await userLogin.load();
+
+        if ( ! userLogin.id ) {
+            return done(null, false);
+        }
+
+        const passwordObj = new Password(password);
+
+        if ( !passwordObj.check(userLogin.password) ) {
+            return done(null, false);
+        }
+
+        return done(null, userLogin.user_id);
+    }
+);
